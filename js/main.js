074d9241f0a68926129964ed9bffa905a01ae931
bhaskarjha514 <bhaskarjha514@gmail.com>
2023-05-16 21:13:56 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -9,9 +9,27 @@ var remoteStream;
 var turnReady;
 
 var pcConfig = {
-  'iceServers': [{
-    'urls': 'stun:stun.l.google.com:19302'
-  }]
+  'iceServers': [
+    { url: 'stun:stun01.sipphone.com' },
+    { url: 'stun:stun.ekiga.net' },
+    { url: 'stun:stunserver.org' },
+    { url: 'stun:stun.softjoys.com' },
+    { url: 'stun:stun.voiparound.com' },
+    { url: 'stun:stun.voipbuster.com' },
+    { url: 'stun:stun.voipstunt.com' },
+    { url: 'stun:stun.voxgratia.org' },
+    { url: 'stun:stun.xten.com' },
+    {
+      url: 'turn:192.158.29.39:3478?transport=udp',
+      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      username: '28224511:1379330808'
+    },
+    {
+      url: 'turn:192.158.29.39:3478?transport=tcp',
+      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      username: '28224511:1379330808'
+    }
+  ]
 };
 
 // Set up audio and video regardless of what devices are present.
